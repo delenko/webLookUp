c3003870a5b325fa0f350c2b90d8b5f78aa64caf
Travis.P <34238377+delenko@users.noreply.github.com>
2019-07-29 04:19:58 -0700

    Update Main.java
    Removed website. Updated code.

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
-        String mangaURL = "https://mangakakalot.com/";
-        Document doc = Jsoup.connect(mangaURL).get();
-        Element tableInfo = doc.getElementsByClass("sts sts_1").first();
+        String URL = "";
+        Document doc = Jsoup.connect(URL).get();
+        Element tableInfo = doc.getElementsByClass("").first();
         String href = tableInfo.attr("href");
         System.out.println(href);
         }
